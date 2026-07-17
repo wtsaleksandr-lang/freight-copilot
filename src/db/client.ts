278d57ceb007c5074f0aa40b48b958ccf06275f4
Alex <wts.aleksandr@gmail.com>
2026-07-17 16:57:52 -0400

    Keep database client typing on core schema

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,10 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { loadEnv } from '../config.js';
-import * as coreSchema from './schema.js';
-import * as shipmentOperationsSchema from './shipmentOperationsSchema.js';
-
-const schema = { ...coreSchema, ...shipmentOperationsSchema };
+import * as schema from './schema.js';
 
 export type DbClient = ReturnType<typeof drizzle<typeof schema>>;
 
